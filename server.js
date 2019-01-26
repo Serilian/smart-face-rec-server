f9f62fb66ef8ba7c570ba207b4531ff188008e36
fhagno@yahoo.com <fhagno@yahoo.com>
2019-01-26 19:29:58 +0100

    Changed port to env.port

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.put('/image', image.imageHandler(db));
 
 app.post('/imageUrl', (req, res) => {image.handleAPICall(req, res)});
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
